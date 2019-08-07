8af31bd279c5fa96efbfa20f62df707deafd049d
rakannimer <rakannimer@gmail.com>
2019-08-07 11:02:41 +0300

    More stuff

--- src/actions.ts
+++ src/actions.ts
@@ -1,25 +1,28 @@
-import memoize from "memoize-one";
+import memoize from "lodash.memoize";
 import nanoid from "nanoid";
 
 import { createChannel } from "./models/Channels";
 import { Dispatcher, ChannelType } from "./types";
 
-export const getActions = memoize((dispatch: Dispatcher) => {
-  const addChannel = (name: string, myId: string) => {
-    const channel: ChannelType = {
-      id: nanoid(),
-      name,
-      createdAt: `${Date.now()}`,
-      updatedAt: `${Date.now()}`,
-      creatorId: myId,
-      messages: { items: [], nextToken: "" }
+export const getActions = memoize(
+  (dispatch: Dispatcher) => {
+    const addChannel = (name: string, myId: string) => {
+      const channel: ChannelType = {
+        id: nanoid(),
+        name,
+        createdAt: `${Date.now()}`,
+        updatedAt: `${Date.now()}`,
+        creatorId: myId,
+        messages: { items: [], nextToken: "" }
+      };
+      dispatch({
+        type: "prepend-channel",
+        payload: channel
+      });
+      createChannel(channel);
     };
-    dispatch({
-      type: "prepend-channel",
-      payload: channel
-    });
-    createChannel(channel);
-  };
-  const loadMoreMessages = (nextToken: string) => {};
-  return { addChannel };
-});
+    const loadMoreMessages = (nextToken: string) => {};
+    return { addChannel };
+  },
+  dispatch => dispatch
+);
