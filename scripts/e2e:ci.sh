c7066de3ba4787740e054504eace513c26910cb0
rakannimer <rakannimer@gmail.com>
2019-08-12 10:49:25 +0300

    Debugging CI added amplify init

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -1,1 +1,3 @@
-yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn amplify mock api
+#!/bin/bash
+
+yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn kill-amplify-mock-api && (amplify mock api &) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
