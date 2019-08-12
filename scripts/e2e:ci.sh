53b1d990d86ec39253411b41c040b7de87ba1199
rakannimer <rakannimer@gmail.com>
2019-08-12 12:27:03 +0300

    Retry amplify mock api after install java

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-java --version
-
-
-# yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn kill-amplify-mock-api && (amplify mock api &) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
+yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn kill-amplify-mock-api && (amplify mock api &) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
