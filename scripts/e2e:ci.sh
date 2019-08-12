3cc030822a2287efad4e3a997cf3162c08184a29
rakannimer <rakannimer@gmail.com>
2019-08-12 11:23:55 +0300

    Debugging ci amplify java installing jre

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+yum install java-1.8.0-openjdk
 java --version
 
 # yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn kill-amplify-mock-api && (amplify mock api &) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
