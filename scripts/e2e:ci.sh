b17df873f08ec3bc505b447c957e51bb6ecc727a
rakannimer <rakannimer@gmail.com>
2019-08-14 14:15:18 +0300

    Adding aws auth

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes 
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"us-east-1\"\
+}"
+
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+yarn amplify init \
+--providers $PROVIDERS \
+--amplify "{ \"envName\":\"ci\" }"\
+--yes
+#yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes 
 yarn kill-amplify-mock-api
 yarn kill-port 62224
 yarn amplify mock api
