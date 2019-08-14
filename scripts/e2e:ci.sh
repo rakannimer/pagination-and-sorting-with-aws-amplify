f416d756bed5e8d84dd5a612a2eb0669f2e6ec6a
rakannimer <rakannimer@gmail.com>
2019-08-14 17:32:37 +0300

    remove bin bash

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -76,7 +76,7 @@
 
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-/bin/bash ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $AWS_REGION
