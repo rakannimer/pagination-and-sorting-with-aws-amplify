22061a450fe4f1828d9a035f9fba5e7d127da7a1
rakannimer <rakannimer@gmail.com>
2019-08-14 13:35:53 +0300

    Add complete ci test script & circleci config

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn kill-amplify-mock-api && yarn kill-port 62224 && amplify mock api #) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
+yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes 
+yarn kill-amplify-mock-api
+yarn kill-port 62224
+amplify mock api
+wait-on http-get://localhost:20002
+yarn build
+kill-port 5000
+(npx serve build/ &)
+wait-on http-get://localhost:5000
+cypress run --env PORT=5000
