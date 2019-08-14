426de4b6e219c61663f119f987394956d65b2c51
rakannimer <rakannimer@gmail.com>
2019-08-14 13:22:01 +0300

    Debugging ci install lsof in system

--- scripts/e2e:ci.sh
+++ scripts/e2e:ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes # && yarn kill-amplify-mock-api && yarn kill-port 62224 && NODE_DEBUG=dynamodb-emulator amplify mock api #) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
+yarn amplify init --amplify "{ \"envName\":\"ci\" }" --yes && yarn kill-amplify-mock-api && yarn kill-port 62224 && amplify mock api #) && wait-on http-get://localhost:20002 && kill-port 5000 && yarn build && (npx serve build/ &) && wait-on http-get://localhost:5000 && PORT=5000 cypress run --env PORT=5000
